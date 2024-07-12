d3f89e70ae97b7c7ef15faffa23904bda0e29f04
Osvin Mazon Guzman <70389790+ossmazon@users.noreply.github.com>
2024-07-12 21:29:01 +0000

    fixed dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo"============"
+echo "============"
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
@@ -10,4 +10,4 @@ python3 /usr/bin/feed.py
 git add -A && git commit -m "Update Feed"
 git push --set-upstream origin main
 
-echo"============"
+echo "============"
